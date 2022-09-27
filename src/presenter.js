6654388454fbd5c6ea113dd50f8888466808d290
Melina <melina.gutierrez@ucb.edu.bo>
2022-09-27 02:42:03 -0400

    crear presentar para el autodirigido

--- src/presenter.js
+++ src/presenter.js
@@ -1,1 +1,12 @@
 import Auto from "./sumador.js";
+
+let cad = document.querySelector("#cadena");
+const button = document.querySelector("#addButton");
+const res = document.querySelector("#resultado");
+let auto
+button.addEventListener("click", (event) => {
+  event.preventDefault();
+  auto = new Auto();
+  let respuesta = auto.ejecutar(cad.value)
+  res.innerHTML = "<p>" + respuesta + "</p>" 
+});
